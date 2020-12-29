061b5f80021a17c2dee1fc413208ef4173f0e036
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2020-12-29 16:38:54 +0300

    Initial Commit

---
+++ PrayerTimes/PrayerTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimesApp.swift
+//  PrayerTimes
+//
+//  Created by Batool  Jawaheri  on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
